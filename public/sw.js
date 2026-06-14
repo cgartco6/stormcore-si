d356be24a5ab45c9a44cd7acace609631b8cc991
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-06-14 21:50:05 +0200

    Add service worker for caching assets

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'stormcore-v2-cache';
+const ASSETS = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
